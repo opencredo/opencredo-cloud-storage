5381f020dd65f19fb9a8e023a7acc45445e6d971
erenay <erenaykin@yahoo.com>
2009-10-23 11:30:34 +0100

    corrections

--- si-s3-adapter/src/main/java/com/opencredo/integration/s3/S3ObjectFormatter.java
+++ si-s3-adapter/src/main/java/com/opencredo/integration/s3/S3ObjectFormatter.java
@@ -1,23 +1,19 @@
 package com.opencredo.integration.s3;
 
+import org.jets3t.service.model.S3Object;
 import org.springframework.integration.annotation.MessageEndpoint;
 import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.integration.core.Message;
-import org.springframework.integration.message.MessageBuilder;
+//import org.springframework.integration.message.MessageBuilder;
 
 
 @MessageEndpoint
 public class S3ObjectFormatter {
 
-	@ServiceActivator(inputChannel="s3InputChannel2", outputChannel="s3OutputMessageChannel")
-	public Message<?> convertS3(Object s3Object){
+	@ServiceActivator(inputChannel="s3InputChannel", outputChannel="s3OutputMessageChannel")
+	public Message handleS3Message(Message<S3Object> s3Object){
 	
-		//convert s3 objects to spring integration messages and sends them to outputChannel
-        
-        //MessageBuilder builder = MessageBuilder.withPayload(s3Object.getMessageBody());
-
-        //decide what headers we want to map
-        //return builder.build();
+        //TODO: handle Message<S3Object>
 		
 		return null;
 	}
