5d786c146f1ef95eb7f5ef0494ea7cb6a6134c84
erenay <erenaykin@yahoo.com>
2009-10-28 21:04:17 +0000

    some corrections

--- si-s3-adapter/test/main/java/com/opencredo/integration/s3/S3FileReadingMessageSourceTest.java
+++ si-s3-adapter/test/main/java/com/opencredo/integration/s3/S3FileReadingMessageSourceTest.java
@@ -1,21 +1,29 @@
 package com.opencredo.integration.s3;
 
-import org.jets3t.service.S3Service;
-import org.jets3t.service.S3ServiceException;
-import org.jets3t.service.model.S3Bucket;
-import org.jets3t.service.model.S3Object;
-
+import static org.jets3t.service.S3Service.BUCKET_STATUS__MY_BUCKET;
 import static org.junit.Assert.assertNotNull;
-
-import org.junit.Before;
-import org.junit.Test;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.jets3t.service.S3Service.*;
 
+import java.util.Calendar;
+import java.util.Map;
+
+import org.jets3t.service.S3ObjectsChunk;
+import org.jets3t.service.S3Service;
+import org.jets3t.service.S3ServiceException;
+import org.jets3t.service.acl.AccessControlList;
+import org.jets3t.service.impl.rest.httpclient.RestS3Service;
+import org.jets3t.service.model.S3Bucket;
+import org.jets3t.service.model.S3BucketLoggingStatus;
+import org.jets3t.service.model.S3Object;
+import org.jets3t.service.model.S3Owner;
+import org.jets3t.service.security.AWSCredentials;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.integration.core.Message;
+
 
 public class S3FileReadingMessageSourceTest {
 	
@@ -23,23 +31,16 @@ public class S3FileReadingMessageSourceTest {
 	
 	private S3FileReadingMessageSource systemUnderTest;
 	
-	@Mock
-	S3Service s3ServiceMock;
-	
-	@Mock
-	S3Bucket s3BucketMock;
+	private S3Service s3Service;
+	private S3Bucket s3Bucket;
 
 	@Before
-    public void doBeforeEachTestCase() {
-        MockitoAnnotations.initMocks(this);
-        systemUnderTest = new S3FileReadingMessageSource(s3ServiceMock, s3BucketMock);
-    }
- 
-    @Test
-    public void testConnectionToBundle() throws S3ServiceException {
-        when(s3ServiceMock.checkBucketStatus(bucketName)).thenReturn(BUCKET_STATUS__MY_BUCKET);
-        verify(s3ServiceMock).checkBucketStatus(bucketName);
-    }
+    public void doBeforeEachTestCase() throws S3ServiceException {
+        //MockitoAnnotations.initMocks(this);
+		s3Service = new RestS3Service(new AWSCredentials("AKIAJJC4KITQHSAY43MQ","U0H0Psg7aS5qrKpLFqZXFUUOq2rK6l2xAfHxZWTd"));
+		s3Bucket = new S3Bucket("sibucket", "LOCATION_EUROPE");
+        systemUnderTest = new S3FileReadingMessageSource(s3Service, s3Bucket);
+	}
  
     @Test
     public void testReceive() throws S3ServiceException {
