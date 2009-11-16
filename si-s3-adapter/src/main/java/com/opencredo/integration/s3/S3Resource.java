dd26830b1320198c0466168623b71de6aa371034
erenay <erenaykin@yahoo.com>
2009-11-16 17:54:48 +0000

    cleaned todos of s3resource

--- si-s3-adapter/src/main/java/com/opencredo/integration/s3/S3Resource.java
+++ si-s3-adapter/src/main/java/com/opencredo/integration/s3/S3Resource.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.security.NoSuchAlgorithmException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jets3t.service.S3Service;
 import org.jets3t.service.S3ServiceException;
 import org.jets3t.service.impl.rest.httpclient.RestS3Service;
@@ -15,6 +18,7 @@ import org.jets3t.service.model.S3Object;
 import org.jets3t.service.security.AWSCredentials;
 import org.springframework.core.io.Resource;
 import org.springframework.util.Assert;
+import org.springframework.util.StringUtils;
 
 
 public class S3Resource implements Resource{
@@ -24,6 +28,8 @@ public class S3Resource implements Resource{
 	private File s3File = null;
 	private S3Object s3Object = null;
 	public static final AWSCredentials awsCredentials = new AWSCredentials("AKIAJJC4KITQHSAY43MQ", "U0H0Psg7aS5qrKpLFqZXFUUOq2rK6l2xAfHxZWTd");
+	
+	private final Log logger = LogFactory.getLog(S3WritingMessageHandler.class);
 	
 	public S3Resource(String bucketName){
 		try {
@@ -84,18 +90,25 @@ public class S3Resource implements Resource{
 	public void sendS3ObjectToS3() throws S3ServiceException{
 		Assert.notNull(s3Object, "s3File should not be null");
 		s3Service.putObject(s3Bucket, s3Object);
-
 	}
 	
 	public void sendS3ObjectToS3(S3Object s3object) throws S3ServiceException{
 		setS3Object(s3object);
 		sendS3ObjectToS3();
-
 	}
 	
-	public Resource createRelative(String arg0) throws IOException {
-		//TODO:
-		return null;
+	/*
+	 * Changes the key value of the S3Object that resides in the same bucket
+	 */
+	public S3Resource createRelative(String relativePath) throws IOException {
+		S3Resource newResource;
+		newResource = new S3Resource(this.s3Bucket.getName());
+		if (this.getFile() != null){
+			if (logger.isDebugEnabled()) logger.debug("filePath:"+ this.s3File.getPath());
+			newResource.setFile(new File(StringUtils.applyRelativePath(this.s3File.getPath(), relativePath)));
+			return newResource;
+		}
+		else return null;
 	}
 
 	public boolean exists() {
@@ -112,32 +125,51 @@ public class S3Resource implements Resource{
 		return new String("JetS3t Properties: " + s3Service.getJetS3tProperties().toString());
 	}
 
+	/*
+	 * return the URL in place of URI
+	 * @see org.springframework.core.io.Resource#getURI()
+	 */
 	public URI getURI() throws IOException {
-		//TODO:
-		return null;
+		try {
+			return getURL().toURI();
+		} 
+		catch (URISyntaxException e) {
+			if (logger.isDebugEnabled()) e.printStackTrace();
+			return null;
+		}
 	}
 
+	/*
+	 * URL for publicly accessible S3Objects
+	 * @see org.springframework.core.io.Resource#getURL()
+	 */
 	public URL getURL() throws IOException {
-		//TODO:
-		return null;
+		if (this.s3File != null)
+			return new URL("http://"+this.s3Bucket.getName()+".s3.amazonaws.com/"+this.s3File.getName());
+		else if (this.s3Object != null)
+			return new URL("http://"+this.s3Bucket.getName()+".s3.amazonaws.com/"+this.s3Object.getKey());
+		else return null;
 	}
 
+	/*
+	 * This resource does not represent a handle with an open stream,
+	 * However contents of an S3Object can be read as an inputStream.
+	 */
 	public boolean isOpen() {
-		//TODO:
-		return true;
+		return false;
 	}
 
 	public boolean isReadable() {
-		//TODO
 		return true;
 	}
 
 	public long lastModified() throws IOException {
-		//TODO: TimeStamp/long conversion
-		Assert.notNull(s3File.getName(), "File name should be known");
+		Assert.notNull(s3File, "File shouldn't be null");
 		try {
+			if (logger.isDebugEnabled()) logger.debug(s3Service.getObjectDetails(s3Bucket, s3File.getName()).getMetadata("METADATA_HEADER_LAST_MODIFIED_DATE"));
 			return Long.parseLong(s3Service.getObjectDetails(s3Bucket, s3File.getName()).getMetadata("METADATA_HEADER_LAST_MODIFIED_DATE").toString());
-		} catch (S3ServiceException e) {
+		} 
+		catch (S3ServiceException e) {
 			e.printStackTrace();
 			return 0;
 		}
@@ -151,7 +183,6 @@ public class S3Resource implements Resource{
 			e.printStackTrace();
 			return null;
 		}
-		
 	}
 
 }
