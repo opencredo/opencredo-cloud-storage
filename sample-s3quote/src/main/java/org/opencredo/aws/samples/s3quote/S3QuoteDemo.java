393c9f6dd195ff8320a1044a657c5a6edd64b9b1
Tomas Lukosius <tomas.lukosius@opencredo.com>
2010-01-29 16:57:22 +0000

    Reviewed Quote demo.

--- sample-s3quote/src/main/java/org/opencredo/aws/samples/s3quote/S3QuoteDemo.java
+++ sample-s3quote/src/main/java/org/opencredo/aws/samples/s3quote/S3QuoteDemo.java
@@ -15,31 +15,37 @@
 
 package org.opencredo.aws.samples.s3quote;
 
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.message.MessageBuilder;
 
 /**
  * @author Eren Aykin (eren.aykin@opencredo.com)
  */
 public class S3QuoteDemo {
-	
-	public static void main(String[] args) {
-		new ClassPathXmlApplicationContext("s3quoteDemo.xml", S3QuoteDemo.class);
-		
-		/*
-		S3TickerUploader tickerUploader = (S3TickerUploader) context.getBean("s3TickerUploader");
-		//sending requests for tickers to be uploaded to S3, so that they can later be read by another application.
-		//can be commented out if already uploaded enough information
-		for (int i = 0; i <4; i++) {
-			char[] chars = new char[3];
-			for (int j = 0; j < 3; j++) {
-				chars[j] = (char) (new Random().nextInt(25) + 65);
-			}
-			String ticker = new String(chars);
-			if (logger.isDebugEnabled()) logger.debug("ticker to upload: "+ticker);
-			MessageBuilder<String> builder = MessageBuilder.withPayload(ticker);
-			tickerUploader.sendTicker(builder.build());
-		}
-		*/
+    private final static Logger LOG = LoggerFactory.getLogger(S3QuoteDemo.class);
 
-	}
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("s3quoteDemo.xml",
+                S3QuoteDemo.class);
+        
+        S3TickerUploader tickerUploader = (S3TickerUploader) context.getBean("s3TickerUploader");
+        // sending requests for tickers to be uploaded to S3, so that they can
+        // later be read by another application.
+        // can be commented out if already uploaded enough information
+        for (int i = 0; i < 4; i++) {
+            char[] chars = new char[3];
+            for (int j = 0; j < 3; j++) {
+                chars[j] = (char) (new Random().nextInt(25) + 65);
+            }
+            String ticker = new String(chars);
+            LOG.info("ticker to upload: {}", ticker);
+            MessageBuilder<String> builder = MessageBuilder.withPayload(ticker);
+            tickerUploader.sendTicker(builder.build());
+        }
+
+    }
 }
